a0070baaf6c23056c55e2ae7e8482398bafcf4b3
mohini-442 <mohinikarwasra598@gmail.com>
2024-04-29 16:45:10 -1200

    updates

--- src/components/Useref.jsx
+++ src/components/Useref.jsx
@@ -4,6 +4,13 @@ import { useState, useEffect, useRef } from "react";
 const Useref = () => {
     const [inputValue, setInputValue] = useState("");
     const count = useRef(0);
+   
+    let ref = useRef(0);
+
+    function handleClick() {
+        ref.current = ref.current + 1;
+        alert('You clicked ' + ref.current + ' times!');
+    }
 
     useEffect(() => {
         count.current = count.current + 1;
@@ -13,13 +20,18 @@ const Useref = () => {
             <div className='container mx-auto py-5'>
                 <div className="d-flex flex-column align-items-center">
                     <h1 className='text-center mb-0 text-danger'>Useref</h1>
+                    <h4 className='mt-4'>render count</h4>
                     <input
                         type="text"
                         value={inputValue}
-                        className='mt-4'
+                    
                         onChange={(e) => setInputValue(e.target.value)}
                     />
                     <h2>Render Count: {count.current}</h2>
+                    <h4 className='mt-4'>click counter</h4>
+                    <button className='btn-1' onClick={handleClick}>
+                        Click me!
+                    </button>
                 </div>
             </div>
         </div>
